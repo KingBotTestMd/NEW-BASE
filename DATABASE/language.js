903e12fefa6f9ab3c5c3077ad1251fb84c90c93d
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-09-03 08:45:10 +0530

    ⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠

--- DATABASE/language.js
+++ DATABASE/language.js
@@ -1,1 +1,19 @@
+const Config = require('./config');
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./DATABASE/Languages/' + Config.LANG + '.json')) {
+    console.log(chalk.green.bold('Loading ' + Config.LANG + ' language...'));
+    var json = JSON.parse(fs.readFileSync('./DATABASE/Languages/' + Config.LANG + '.json'));
+} else {
+    console.log(chalk.red.bold('You entered an invalid language. English language was chosen.'));
+    var json = JSON.parse(fs.readFileSync('./DATABASE/Languages/EN.json'));
+}
+
+function getString(file) { return json['STRINGS'][file]; }
+
+module.exports = {
+    language: json,
+    getString: getString
+}
 
