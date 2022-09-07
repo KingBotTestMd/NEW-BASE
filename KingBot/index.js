dc1cc3d6555de3844888c0fe1e0584578b4e9e7f
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-09-07 19:01:08 +0530

    ⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠

--- KingBot/index.js
+++ KingBot/index.js
@@ -1,1 +1,15 @@
+/* Copyright (C) 2022 WHITE HACKERS
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ MD BOT
+*/
 
+
+module.exports = {
+    Base: require('./_Base'),
+    Message: require('./_Message'),
+    StringSession: require('./_StringSession'),
+    ReplyMessage: require('./_ReplyMessage'),
+    Image: require('./_Image'),
+    Video: require('./_Video')
+};
