4c6178736ee45ee57e57e07f3975fc29b8711aff
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-10-06 16:19:44 +0530

    Update alive.js

--- Commands/alive.js
+++ Commands/alive.js
@@ -1,5 +1,5 @@
 const {addCMD} = require('cobra-event-emit').events;
-const KingBot = require('../events');
+const KingBot = require('../DATABASE/events');
 
 KingBot.KingXCMD({pattern: 'test$', desc: '', fromMe: true}, (async (message,  match) => {
           await message.client.sendMessage(message.jid, {text: "Hello Bot Is Working !!!😋"}, {quoted: message.data})
