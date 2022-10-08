a6cb683d2d6cb2f84ca3ef0610c5aabd7cfdec1f
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-10-08 11:04:49 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -120,9 +120,8 @@ events.commands.map(
                         command.pattern.test(text_msg))))) {
 // ════════════════════VIDEO & IMAGE
                     let sendMsg = false;
-              		const chatId = message.key.remoteJid
                    	let data = makeBufferData()
-		            const chat = data.chatUpdates[chatId] || data.chatUpserts[chatId]
+		            const chat = data.chatUpdates[message.key.remoteJid] || data.chatUpserts[message.key.remoteJid]
 //                    var chat = KingBot.chats.get(msg.key.remoteJid)
                         
                     if ((Config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
