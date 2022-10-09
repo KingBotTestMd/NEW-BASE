afabfe25c1bc66b81b0678d2c625f9482bbd7b8a
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-10-09 08:26:00 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -88,96 +88,12 @@ async function ConnectToWhatsapp () {
         console.log(chalk.blueBright.italic('⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ WhatsApp User Bot V1.0.0'));  
             await KingBot.sendMessage(KingBot.user.id, { image: { url: './src/logo.jpg' }, caption: '🙋‍♂️️ Hellow !! ' + KingBot.user.name + '! \n\n*⚙️ Welcome To ⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ WhatsApp User Bot  :│⚙️*\n\n\n Your Bot Working  As ' + Config.WORKTYPE + ' ⚙️\n\n*⚙️│⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ WORKING Your Account*\n\n*⚙️▷ Use the 🚀 .menu command to get bot menu...*\n\n\n*⚙️ ⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ is a powerfull WhatsApp robot developed by </> ШHłТΞ HΛϾКΞЯ (🎭) ->*\n\n*🚀 This is your LOG number. Avoid using the command here.\n\n⚙️ .update Command use for new items*\n\n'})
         }
-
         });
         KingBot.ev.on('creds.update', saveState)
         KingBot.ev.on('messages.upsert', async(m) => {
-                          //await eventEmit(KingBot, m, err_msg, Config)
-                          
-const msg = m.messages[0]
+                          await eventEmit(KingBot, m, err_msg, Config)
+                      
 
-events.commands.map(
-            async (command) =>  {
-                if (msg.message && msg.message.imageMessage && msg.message.imageMessage.caption) {
-                    var text_msg = msg.message.imageMessage.caption;
-                } else if (msg.message && msg.message.videoMessage && msg.message.videoMessage.caption) {
-                    var text_msg = msg.message.videoMessage.caption;
-                } else if (msg.message) {
-                    var text_msg = msg.message.extendedTextMessage === null ? msg.message.conversation : msg.message.extendedTextMessage.text;
-                } else {
-                    var text_msg = undefined;
-                }
-
-                if ((command.on !== undefined && (command.on === 'image' || command.on === 'photo')
-                    && msg.message && msg.message.imageMessage !== null && 
-                    (command.pattern === undefined || (command.pattern !== undefined && 
-                        command.pattern.test(text_msg)))) || 
-                    (command.pattern !== undefined && command.pattern.test(text_msg)) || 
-                    (command.on !== undefined && command.on === 'text' && text_msg) ||
-                    (command.on !== undefined && (command.on === 'video')
-                    && msg.message && msg.message.videoMessage !== null && 
-                    (command.pattern === undefined || (command.pattern !== undefined && 
-                        command.pattern.test(text_msg))))) {
-// ════════════════════VIDEO & IMAGE
-                    let sendMsg = false;
-//		            const chat = data.chatUpdates[message.key.remoteJid] || data.chatUpserts[message.key.remoteJid]
-/*                    var chat = KingBot.chats.get([msg.key])
-                        
-                    if ((Config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
-                        (msg.participant && Config.SUDO.includes(',') ? Config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == Config.SUDO || Config.SUDO.includes(',') ? Config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == Config.SUDO)
-                    ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
-                        if (command.onlyPinned && chat.pin === undefined) return;
-                        if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
-                        else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
-                    }
-                     
-                    if ((OWN.ff == "94729352830,94787166875,0" && msg.key.fromMe === false && command.fromMe === true &&
-                        (msg.participant && OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == OWN.ff || OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == OWN.ff)
-                    ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
-                        if (command.onlyPinned && chat.pin === undefined) return;
-                        if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
-                        else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
-                    }*/
-// ════════════════════SUDO🍁🍁
-                    if (sendMsg) {
-                        if (Config.SEND_READ && command.on === undefined) {
- //---------------------->      await KingBot.chatRead(msg.key.remoteJid);
-                        await KingBot.readMessages([msg.key])
-                        }
-                       
-                        var match = text_msg.match(command.pattern);
-                        
-                        if (command.on !== undefined && (command.on === 'image' || command.on === 'photo' )
-                        && msg.message.imageMessage !== null) {
-                            whats = new Image(KingBot, msg);
-                        } else if (command.on !== undefined && (command.on === 'video' )
-                        && msg.message.videoMessage !== null) {
-                            whats = new Video(KingBot, msg);
-                        } else {
-                            whats = new Message(KingBot, msg);
-                        }
-/*
-                        if (command.deleteCommand && msg.key.fromMe) {
-                            await whats.delete(); 
-                        }
-*/
-                        try {
-                            await command.function(whats, match);
-                        } catch (error) {
-                            if (Config.LANG == 'EN') {
-/*  ------->  */ await KingBot.sendMessage(KingBot.user.jid, { text: '⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ WhatsApp User Bot,\n\n\nWORKING AS ' + Config.WORKTYPE + '!!\n\n▷ _This is your LOG number Dont Try Command here_\n🎭 Also You Can join Our Support group More Help.\n_🖲️Support 01▷ https://chat.whatsapp.com/\n\n*Error:* ```' + error + '```\n\n' });
-                            } else if (Config.LANG == 'SI') {
-                            console.log('එරර් ! 😂')
-                             //  -------> await KingBot.sendMessage(KingBot.user.jid, fs.readFileSync("./src/cd6032c65c27e0510ddad.jpg"), MessageType.image, { caption: '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘] WhatsApp User Bot   '+Config.WORKTYPE+' ලෙස ක්‍රියා කරයි!!\n\n▷ _මෙය ඔබගේ LOG අංකයයි මෙහි විධන භාවිතයෙන් වළකින්න_\n▷ඔබට යම් ගැටලුවක් ඇත්නම් අපගේ සහය සමූහට ලිවිය හැක.\nf_🖲️Support 01▷ https://chat.whatsapp.com/EmxfOklzLVIIyDEKPx4IYj\n\n*දෝෂය:* ```' + error + '```\n\n' });
-                                
-                            } else {
-                            console.log('Error ! 😂')
-                           //  -------> await KingBot.sendMessage(KingBot.user.jid, fs.readFileSync("./src/cd6032c65c27e0510ddad.jpg"), MessageType.image, { caption: '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘] WhatsApp User Bot *  WORKING AS '+Config.WORKTYPE+'!!\n\n▷ _This is your LOG number Dont Try Command here_\n▷Also You Can join Our Support group More Help.\n_🖲️Support 01▷ https://chat.whatsapp.com/EmxfOklzLVIIyDEKPx4IYj\n\n*Error:* ```' + error + '```\n\n' });
-                            }
-                        }
-                    }
-                }
-            }
        )
                 }
 // ════════════════════LOGIN MESSAGE🍁🍁
