bda2e28943f78a7af027eeb5cfc01563e905cef0
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2022-10-14 20:53:22 +0530

    Update AddCMD.js

--- Commands/AddCMD.js
+++ Commands/AddCMD.js
@@ -1,16 +1,7 @@
-/*
-░█████╗░░█████╗░██████╗░██████╗░░█████╗░░░░░░░███╗░░░███╗██████╗░
-██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗░░░░░░████╗░████║██╔══██╗
-██║░░╚═╝██║░░██║██████╦╝██████╔╝███████║█████╗██╔████╔██║██║░░██║
-██║░░██╗██║░░██║██╔══██╗██╔══██╗██╔══██║╚════╝██║╚██╔╝██║██║░░██║
-╚█████╔╝╚█████╔╝██████╦╝██║░░██║██║░░██║░░░░░░██║░╚═╝░██║██████╔╝
-░╚════╝░░╚════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░░░░╚═╝░░░░░╚═╝╚═════╝░ BY SISULA WELGAMAGE
-
-Coded By  Sisula welgamage | 2022/08/08 Main Release | Docker Image : sisula/whatsapp_bot | Host: AWS(Heroku) OR etc. | Terminal: Node 
-*/
+const KingBot = require('../DATABASE/events');
 const {addCMD} = require('cobra-event-emit').events;
 
-addCMD({pattern: 'test$', desc: '', fromMe: true}, (async (message,  match) => {
-          await message.client.sendMessage(message.jid, {text: "Hello Bot Is Working !!! Sisula Welgamage"}, {quoted: message.data})
+KingBot.KingXCMD({pattern: 'test$', desc: '', fromMe: true}, (async (message,  match) => {
+          await message.client.sendMessage(message.jid, {text: "Hello Bot Is Working !!!"}, {quoted: message.data})
 
 }))
