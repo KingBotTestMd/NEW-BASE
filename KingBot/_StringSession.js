c7ab4d96c4c9ee299e668e7481e6522afe6018be
KingBotTestMd <111508807+KingBotTestMd@users.noreply.github.com>
2023-03-23 17:59:02 +0530

    Add files via upload

---
+++ KingBot/_StringSession.js
@@ -0,0 +1,34 @@
+/* Copyright (C) 2022 WHITE HACKERS
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+⎝🎭 𝚂𝙻 𝙺𝙸𝙽𝙶 𝚇 🎭⎠ MD BOT
+*/
+
+
+const fs = require('fs');
+
+class StringSession {
+    constructor() {
+    }
+
+    deCrypt(string = undefined) {
+        if ('SLKINGX_SESSION' in process.env && string === undefined) {
+            string = process.env.STRING_SESSION;
+        } else if (string !== undefined) {
+            if (fs.existsSync(string)) {
+                string = fs.readFileSync(string, {encoding:'utf8', flag:'r'});
+            }
+        }
+        
+        var split = string.split(';;;');
+        if (split.length >= 2) {
+            return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
+        }
+    }
+
+    createStringSession(dict) {
+        return 'SLKINGX;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+    }
+}
+
+module.exports = StringSession;
